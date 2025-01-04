be97102d3e65e061cc304e9316eb5abbf28fdf4c
Thendelo Romario Mashapha <romariomashinquiries@gmail.com>
2025-01-04 23:59:56 +0200

    ok

--- playlistdownloaer/playlistdownloaer/settings.py
+++ playlistdownloaer/playlistdownloaer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w-#30j7%x2m2nuv_hwnoir+dj58^#7@p^eb8#zwg&39s^u%c2x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
