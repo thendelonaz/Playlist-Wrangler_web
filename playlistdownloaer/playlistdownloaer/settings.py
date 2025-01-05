d82cff10acf333032a5a6ef1bce2df381ab0325b
Thendelo Romario Mashapha <romariomashinquiries@gmail.com>
2025-01-05 14:05:05 +0200

    last try

--- playlistdownloaer/playlistdownloaer/settings.py
+++ playlistdownloaer/playlistdownloaer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['https://playlist-wrangler-web.vercel.app/','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'downloader',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,9 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[
-    BASE_DIR / "static"
-]
+#STATICFILES_DIRS =[BASE_DIR / "static"]
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'downloader/static')]
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'staticfiles')]
+
 MEDIA_URL ='media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
